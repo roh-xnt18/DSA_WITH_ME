382e40c8eab3ae7c9bb8364e8f82bc9b083e40bc
Rohan Kumar <rohankr77800@gmail.com>
2025-08-15 22:51:28 +0530

    Add files via upload

---
+++ SORTING/selection_sort.java
@@ -0,0 +1,29 @@
+package SORTING;
+
+public class selection_sort {
+     public static void selectionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+           
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {64, 25, 12, 22, 11};
+
+        selectionSort(arr);
+
+        System.out.print("Sorted array: ");
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
