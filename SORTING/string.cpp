9ca1c8bef930f9fc779aea43134abcbdd563ff34
Rohan Kumar <rohankr77800@gmail.com>
2025-08-17 23:54:11 +0530

    Add files via upload

---
+++ SORTING/string.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "sortingexample";
+    
+    sort(s.begin(), s.end()); 
+    
+    cout << "Sorted characters in string: " << s << endl;
+    return 0;
+}
