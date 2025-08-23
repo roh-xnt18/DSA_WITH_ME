d3319eb2f04aba900a6dec212d71357207d2979c
Rohan Kumar <rohankr77800@gmail.com>
2025-08-23 23:31:29 +0530

    Add files via upload

---
+++ Arrays/frequency.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;  // size of array
+    
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    unordered_map<int, int> freq;
+
+    for (int x : arr) {
+        freq[x]++;
+    }
+
+    cout << "Element frequencies:\n";
+    for (auto &p : freq) {
+        cout << p.first << " -> " << p.second << "\n";
+    }
+
+    return 0;
+}
