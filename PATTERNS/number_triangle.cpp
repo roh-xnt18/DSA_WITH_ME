c3486a5f7d7dbb00ef504493d2dcc278ab41d7a0
Rohan Kumar <rohankr77800@gmail.com>
2025-08-24 23:45:34 +0530

    Update number_triangle.cpp

--- PATTERNS/number_triangle.cpp
+++ PATTERNS/number_triangle.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int n = 5; // number of rows
+    int n = 5;
 
     for (int i = n; i >= 1; i--) {
         for (int j = 1; j <= i; j++) {
@@ -11,4 +11,5 @@ int main() {
         cout << endl;
     }
     return 0;
+
 }
