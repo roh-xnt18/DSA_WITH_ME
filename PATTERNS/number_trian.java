d5cc942306f7f5c4bed0d5ecf7169eac12b9606c
Rohan Kumar <rohankr77800@gmail.com>
2025-08-24 23:45:14 +0530

    Update number_trian.java

--- PATTERNS/number_trian.java
+++ PATTERNS/number_trian.java
@@ -1,6 +1,6 @@
 public class number_trian {
     public static void main(String[] args) {
-        int n = 5; // number of rows
+        int n = 5; 
 
         for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
@@ -10,3 +10,4 @@ public class number_trian {
         }
     }
 }
+
