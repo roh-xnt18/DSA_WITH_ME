b44314d87adcc95c4df74375b8fdba6c9dd8610b
Rohan Kumar <rohankr77800@gmail.com>
2025-08-25 23:32:31 +0530

    Update factorial.cpp

--- RECURSION/factorial.cpp
+++ RECURSION/factorial.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int factorial(int n) {
-    // base case
+
     if (n == 0 || n == 1)
         return 1;
 
-    // recursive call
+    
     return n * factorial(n - 1);
 }
 
@@ -15,3 +15,4 @@ int main() {
     cout << "Factorial of " << num << " is: " << factorial(num);
     return 0;
 }
+
